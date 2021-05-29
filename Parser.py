5f5cc6eba878fad8e7c12483194d84987a30c48f
Madhav <madhav16159@iiitd.ac.in>
2021-05-29 21:29:33 +0530

    dictionary to store all citations for author

--- Parser.py
+++ Parser.py
@@ -1,20 +1,38 @@
+from logging import debug
+from os import name
+from typing import Dict
 from scholarly import scholarly, ProxyGenerator
 import json
 import pandas as pd
 
 class Parser:
 
-    def __init__(self, author_id: str, name: str) -> None:
+    def __init__(self, author_id: str, name: str, debug=False) -> None:
         self.author_id = author_id
         self.name = name
+        self.debug = debug
+    
+    def get_all_citations(self) -> Dict:
+        author_cite_times = {}
 
-    def get_all_papers(self):
         search_query = scholarly.search_author_id(self.author_id)
         author = scholarly.fill(search_query)
         for publication in author["publications"]:
-            print(publication["bib"]["title"])
+            if self.debug:
+                print("Publication - ", publication["bib"]["title"])
 
-# p = Parser(author_id="-DAxp-cAAAAJ", name="Mayank Vatsa")
-p = Parser(author_id="eZ4WZmIAAAAJ", name="Anish Madan")
+            detailed_publication = scholarly.fill(publication)
+            cited_this = scholarly.citedby(detailed_publication)
 
-p.get_all_papers()
+            for each_citation in cited_this:
+                if self.debug:
+                    print("Citation for current paper - ", each_citation['bib'])
+                all_authors_this_citations =  each_citation['bib']["author"].split(" and ")
+                for each_author in all_authors_this_citations:
+                    author_cite_times[each_author] += 1
+
+        return author_cite_times
+
+if __name__ == "__main__":
+    psr = Parser(author_id="eZ4WZmIAAAAJ", name="Anish Madan", debug=True)
+    print(psr.get_all_citations())
